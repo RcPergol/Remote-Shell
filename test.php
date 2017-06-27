091040b7ffed18fc0a9a9a6e82c1ae4d3f68f3af
RcPergol <karol.pergol@o2.pl>
2017-06-27 13:42:06 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,11 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+<h1>elo </h1>
+  <?php
+$rcp =  file_get_contents("php://input");
+echo $rcp;
+echo ="chron";
+  ?>
  </body>
 </html>
